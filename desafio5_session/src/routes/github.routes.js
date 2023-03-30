fa57e998a5806215be2a6825eb63037020b79f5d
Martin Suarez <martin086@gmail.com>
2023-03-30 17:20:50 -0300

    Pulimos cards de products y ruta de acceso via GitHub.

--- desafio5_session/src/routes/github.routes.js
+++ desafio5_session/src/routes/github.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import passport from "passport";
 
+
 const routerGithub = Router()
 
 //Register
@@ -9,7 +10,14 @@ routerGithub.get('/github', passport.authenticate('github', {scope:['user:email'
 //Login
 routerGithub.get('/githubSession', passport.authenticate('github'), async (req,res) => {
     req.session.user = req.user
-    res.redirect('/api/products')
+    if(req.session.user) {
+        req.session.login = true
+        req.session.role = "user"
+        res.redirect('/products')
+    } else {
+        res.redirect('/login')
+    }
+    
 })
 
 export default routerGithub
