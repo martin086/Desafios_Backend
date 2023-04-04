20e531d697984c801e7bccda0a60a787d36c5982
Martin Suarez <martin086@gmail.com>
2023-04-04 19:00:39 -0300

    Primer commit práctica integradora n°2.

---
+++ integrador2/src/routes/carts.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { addProductToCart, createCart, emptyCart, getCart, removeProductCart, updateAllCart, updateProdQtyCart } from "../controllers/cart.controller.js";
+
+const routerCart = Router()
+
+routerCart.get('/:cid', getCart)
+routerCart.post('/', createCart)
+routerCart.post('/:cid/products/:pid', addProductToCart)
+routerCart.put('/:cid', updateAllCart)
+routerCart.put('/:cid/products/:pid', updateProdQtyCart)
+routerCart.delete('/:cid/products/:pid', removeProductCart)
+routerCart.delete('/:cid', emptyCart)
+
+
+export default routerCart;
---
+++ integrador2/src/routes/views.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { requireAuth } from "../controllers/session.controller.js";
+import { renderProducts, viewCarts, viewLogin, viewProducts, viewRegister } from "../controllers/view.controller.js";
+
+const routerViews = Router()
+
+routerViews.get('/', requireAuth, viewProducts)
+routerViews.get('/login', viewLogin)
+routerViews.get('/register', viewRegister)
+routerViews.get('/products', requireAuth, renderProducts)
+routerViews.get('/carts/:cid', requireAuth, viewCarts)
+
+export default routerViews
